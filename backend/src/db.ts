7f4258fc884059568c9d9026889dd57644031c7d
Umang Agarwal <umangagarwal257@gmail.com>
2025-03-20 18:28:44 +0530

    Added signup, signin and database

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,6 +4,28 @@ const ObjectId = mongoose.Types.ObjectId;
 
 const userSchema = new Schema({
     username : { type : String , required : true, unique : true},
-    password : { type : String , required : true, unique : true}
+    password : { type : String , required : true}
 })
+
+const tagSchema = new Schema({
+    title : { type : String, required : true, unique : true}
+})
+
+const contentSchema = new Schema({
+    link : {type : String , required : true},
+    type : {type : String , required : true},
+    title : {type : String , required : true},
+    tags : [{type : ObjectId , ref : 'Tag'}],
+    userId : {type : ObjectId , ref : "User", required : true}
+})
+
+const linkSchema = new Schema({
+    hash : {type : String , required : true},
+    userId : {type : ObjectId , ref : "User", required : true}
+})
+
+export const UserModel = mongoose.model("users", userSchema);
+export const TagModel = mongoose.model("tags", tagSchema);
+export const ContentModel = mongoose.model("contents", contentSchema);
+export const LinkModel = mongoose.model("links", linkSchema);
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,94 @@
-
 import express , {Request , Response} from "express"
+import {z} from "zod"
+import { UserModel } from "./db";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+import mongoose from "mongoose";
 
+dotenv.config({ path: __dirname + "/../.env" });
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongourl = process.env.MONGO_URL
 
 app.use(express.json());
 
-app.post("/api/v1/signup", (req : Request , res : Response) => {
-  res.send("Hello World");
+app.post("/api/v1/signup", async (req : Request , res : Response) => {
+  const requiredBody = z.object({
+    username: z.string()
+    .min(2, { message: 'Username must be at least 3 characters' })
+    .max(20, { message: 'Username must be between 3 and 20 characters' }),
+
+    password: z.string()
+    .min(6, { message: 'Password must be at least 6 characters' })
+    .max(20, { message: 'Password must be between 6 and 20 characters' })
+    .regex(
+        /^[A-Za-z\d]+$/,
+        { message: "Password must include at least one letter and one digit"}
+    )   
+  })
+  const parsedDataWithSuccess = requiredBody.safeParse(req.body)
+
+  if (!parsedDataWithSuccess.success) {
+    res.json({
+      message: "Incorrect format",
+    });
+    return;
+  }
+  try {
+    const { username, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    await UserModel.create({
+      username,
+      password: hashedPassword,
+    });
+
+    res.json({
+      message: "You are signed up",
+    });
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: "Error during signup",
+    });
+  }
+
 })
 
-app.post("/api/v1/signin" , (req : Request , res : Response) => {
-  res.send("Hello World");
+app.post("/api/v1/signin" ,async (req : Request , res : Response) => {
+  const { username, password } = req.body;
+
+  const user = await UserModel.findOne({
+    username: username,
+  });
+
+  if (!user) {
+    res.status(401).json({
+      message: "This user doesn't exist",
+    });
+    return;
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (passwordMatch) {
+    const token = jwt.sign(
+      {
+        id: user._id.toString(),
+      },
+      process.env.JWT_USER_PASSWORD!
+    );
+
+    res.json({
+      token,
+    });
+  } else {
+    res.status(401).json({
+      message: "Incorrect credentials",
+    });
+  }
 });
 
 app.post("/api/v1/content" , (req : Request , res : Response) => {
@@ -34,6 +111,12 @@ app.get("/api/v1/brain/:shareLink" , (req : Request , res : Response) => {
   res.send("Hello World");
 });
 
-app.listen(PORT , () => {
-  console.log(`Server listening on port ${PORT}`)
-});
+async function main() {
+  await mongoose.connect(mongourl!);
+  console.log("connected to");
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:3000`);
+  });
+}
+main();
