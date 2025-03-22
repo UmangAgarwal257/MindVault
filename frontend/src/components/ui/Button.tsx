fb44c8d45017b233327e5859aa7805f28b5513c3
Umang Agarwal <umangagarwal257@gmail.com>
2025-03-22 14:28:04 +0530

    add : button

---
+++ frontend/src/components/ui/Button.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+
+
+type Variants = "primary" | "secondary"
+
+interface ButtonProps {
+    variant : Variants;
+    size : "sm" | "md" | "lg";
+    text : string;
+    startIcon? : ReactNode;
+    endIcon? : ReactNode;
+    onClick? : () => void;
+}
+
+const variantStyles  = {
+    "primary" : "bg-purple-600 text-white",
+    "secondary" : "bg-purple-300 text-purple-500"
+}
+
+const sizeStyles = {
+    "sm" : "py-1 px-2",
+    "md" : "py-2 px-4",
+    "lg" : "py-4 px-6"
+}
+
+const defaultStyles = "rounded-md flex"
+
+export const Button = (props : ButtonProps) => {
+  return (
+    <button className={`${variantStyles[props.variant]}
+     ${defaultStyles} ${sizeStyles[props.size]}`}>
+        {props.startIcon? <div className="pr-2">{props.startIcon}</div> : null}
+        {props.text} {props.endIcon}</button>
+  )
+}
+
+<Button variant = "primary" size = "md" onClick={() => {}} text = {"asd"}  />
